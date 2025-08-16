c8d6f48b78d195300e1b8c37e7ca0bb9a4b7edef
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-08-17 00:23:42 +0330

    refactor app module to include middleware and module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+// app.module.ts
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
