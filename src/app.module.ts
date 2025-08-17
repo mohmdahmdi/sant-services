11cc189ff863e882debd86bf1d0466876b45798b
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-08-17 11:25:59 +0330

    refactor: rename logger middleware file and update import path

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { LoggerMiddleware } from './logger/logger.middleware';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
