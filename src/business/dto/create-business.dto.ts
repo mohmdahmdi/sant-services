a21d0a6eb5f1701668efc74c8070a467b906f6b2
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-08-17 19:40:29 +0330

    feat: add validation rules to CreateBusinessDto for improved data integrity

--- src/business/dto/create-business.dto.ts
+++ src/business/dto/create-business.dto.ts
@@ -1,1 +1,61 @@
-export class CreateBusinessDto {}
+import {
+  IsString,
+  IsOptional,
+  IsUUID,
+  IsBoolean,
+  IsEmail,
+} from 'class-validator';
+
+export class CreateBusinessDto {
+  @IsUUID()
+  owner_id: string;
+
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  logo?: string;
+
+  @IsOptional()
+  @IsString()
+  cover_image?: string;
+
+  @IsUUID()
+  location_id: string;
+
+  @IsUUID()
+  business_type_id: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  website?: string;
+
+  @IsOptional()
+  @IsString()
+  instagram?: string;
+
+  @IsOptional()
+  @IsString()
+  whatsapp?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  is_verified?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  is_active?: boolean;
+}
